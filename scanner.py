97602023a009857130643503f399a3a76a228c6e
matthova <matthova87@gmail.com>
2016-06-03 23:09:28 +0000

    fixed it but also broke it

--- scanner.py
+++ scanner.py
@@ -1,19 +1,20 @@
 from time import sleep
 import serial
 
-opening = True
-f = open('test.txt', 'ab+')
+opened = False
 
 with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as ser:
     while True:
-	if opening == False:
+	if opened == True:
 	    ser.write('M105\n')
             sleep(1)
             line = ser.readline()
-            f.write(line)
+            with open('test.txt', 'a') as f:
+                print line
+                f.write(line)
 	else:
             line = ser.readline()   # read a '\n' terminated line
 	    if line == 'echo:  M200 D0\n':
                 print 'done opening'
-                opening = False
+                opened = True
 
