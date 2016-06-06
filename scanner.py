38ef56dfbb246bb14bc521f2068a05c920864018
hovanem <matt.hovanec@autodesk.com>
2016-06-06 11:08:36 -0700

    exchanged file writing for logger library

--- scanner.py
+++ scanner.py
@@ -1,19 +1,21 @@
-from time import sleep
+import logging
 import serial
+from time import sleep
 
 opening = True
-f = open('test.txt', 'ab+')
+
+logging.basicConfig(filename='temp.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as ser:
     while True:
-	if opening == False:
-	    ser.write('M105\n')
-            sleep(1)
-            line = ser.readline()
-            f.write(line)
-	else:
-            line = ser.readline()   # read a '\n' terminated line
-	    if line == 'echo:  M200 D0\n':
-                print 'done opening'
-                opening = False
-
+    if opening == False:
+        ser.write('M105\n')
+        sleep(1)
+        line = ser.readline()
+        f.write(line)
+        logging.info(line)
+    else:
+        line = ser.readline()   # read a '\n' terminated line
+        if line == 'echo:  M200 D0\n':
+            print 'done opening'
+            opening = False
