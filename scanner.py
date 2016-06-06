cffef67adcdac206371dc935d85231d611f89896
matthova <matthova87@gmail.com>
2016-06-06 18:17:45 +0000

    woops

--- scanner.py
+++ scanner.py
@@ -8,14 +8,13 @@ logging.basicConfig(filename='temp.log', level=logging.DEBUG, format='%(asctime)
 
 with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as ser:
     while True:
-    if opening == False:
-        ser.write('M105\n')
-        sleep(1)
-        line = ser.readline()
-        f.write(line)
-        logging.info(line)
-    else:
-        line = ser.readline()   # read a '\n' terminated line
-        if line == 'echo:  M200 D0\n':
-            print 'done opening'
-            opening = False
+        if opening == False:
+            ser.write('M105\n')
+            sleep(1)
+            line = ser.readline()
+            logging.info(line)
+        else:
+            line = ser.readline()   # read a '\n' terminated line
+            if line == 'echo:  M200 D0\n':
+                logging.info('Serial port opened')
+                opening = False
